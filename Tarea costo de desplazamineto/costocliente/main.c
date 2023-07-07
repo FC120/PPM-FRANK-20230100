8b88040c9881afef549ecb3f1d93bb9bc3e226f5
frankcaceres <frankcaceres120@gmail.com>
2023-07-07 09:59:02 +0800

    Tarea

--- Tarea costo de desplazamineto/costocliente/main.c
+++ Tarea costo de desplazamineto/costocliente/main.c
@@ -1,60 +1,79 @@
 #include <stdio.h>
-#define PRECIO_GALON 274.50
-#define litro 3.785
 
-    int main() {
-    char nombre[50];
-    float kilometros, litros,rendimiento;
-    float pago_de_seguro,seguro_por_kilometros;
-    float mantenimineto,precio_de_manteniminto_por_kilometros;
-    float aceite,consumo_de_aceite;
-    float consu_precio,gastos_totales;
-    float conver_litros_galones5,galones5,litros2;
+int main() {
+    char cliente[50];
+    float km_recorridos;
+    float precio_combustible;
+    float cambio_aceite;
+    float cambio_filtros;
+    float cambio_bandas;
+    float seguro;
+    float precio_gomas;
+    float consumo_max = 16.0;
+    float consumo_min = 8.0;
+    float precision = 1.0;
+    float precio_gasolina = 193.0;
+    float litros_por_galon = 3.78541;
 
-    printf("-----BIENVENIDO/A-----\n");
+    printf("POR FAVOR MAXIMIZAR VENTANA\n");
     printf("Ingrese el nombre del cliente: ");
-    scanf("%s", nombre);
+    scanf("%s", cliente);
 
-    printf("Ingrese la cantidad de kilometros recorridos: ");
-    scanf("%f", &kilometros);
+    printf("Ingrese los kilómetros recorridos: ");
+    scanf("%f", &km_recorridos);
 
-    printf("Ingrese la cantidad de litros de gasolina consumidos (minimo 8k maximo 16km): ");
-    scanf("%f", &litros);
-    printf("\n");
+    printf("Ingrese el precio del combustible: ");
+    scanf("%f", &precio_combustible);
 
-    printf("-----Gastos Varios-----");
-    printf("\n");
+    printf("Ingrese el costo del cambio de aceite: ");
+    scanf("%f", &cambio_aceite);
 
-    printf("Ingrese el pago mensual del seguro: ");
-    scanf("%f", &pago_de_seguro);
+    printf("Ingrese el costo del cambio de filtros: ");
+    scanf("%f", &cambio_filtros);
 
+    printf("Ingrese el costo del cambio de bandas: ");
+    scanf("%f", &cambio_bandas);
 
-    printf("Ingrese el precio de mantenimiento del vehiculo: ");
-    scanf("%f", &mantenimineto);
+    printf("Ingrese el costo del seguro anual: ");
+    scanf("%f", &seguro);
 
+    printf("Ingrese el costo de las gomas: ");
+    scanf("%f", &precio_gomas);
 
-    printf("Ingrese la cantidad de litros de aceite que usa el vehiculo: ");
-    scanf("%f", &aceite);
-    printf("\n");
+    // Cálculo de los gastos varios
+    float gastos_mantenimiento = cambio_aceite + cambio_filtros + cambio_bandas;
+    float gastos_varios_km = gastos_mantenimiento + seguro + (precio_gomas * km_recorridos);
+    float gastos_totales = gastos_varios_km * km_recorridos;
 
+    // Cálculo del consumo promedio y costo total
+    float consumo_promedio = 0.0;
+    float costo_total = 0.0;
+    float km_por_galon = 0.0;
 
-    consu_precio = PRECIO_GALON / litros;
-    consumo_de_aceite = aceite / kilometros;
-    precio_de_manteniminto_por_kilometros = mantenimineto / kilometros;
-    rendimiento = kilometros / litros;
-    seguro_por_kilometros = pago_de_seguro / kilometros;
-    gastos_totales = pago_de_seguro + mantenimineto + consu_precio ;
+     FILE *archivo = fopen("resultados.txt", "w");
+    if (archivo == NULL)
+    {
+        printf("No se pudo abrir el archivo.\n");
+        return 1;
+    }
 
-    printf("Cliente: %s\n", nombre);
-    printf("Rendimiento de combustible: %.2f kilometros por litro\n", rendimiento);
-    printf("Rendimiento de combustible en peso por kilometros es: %.2f pesos \n", consu_precio);
-    printf("Pago de seguro por kilometros en pesos : %.2f \n", seguro_por_kilometros);
-    printf("El precio de mantenimiento por kilometros es : %.2f \n",precio_de_manteniminto_por_kilometros);
-    printf("La cantidad de aceite que consume el auto por kilometros es: %.2f litros por kilometros\n",consumo_de_aceite);
-    printf("El total de gastos es: %.2f Pesos\n",gastos_totales);
+    printf("\n-----------------------------------------------------------------------|Cliente|------------------------------------------------------------------------------------: \n   \t\t\t\t\t\t\t\t\t%s\n\n", cliente);
+    printf("\nL/100\nConsumo promedio\tNombre\t\tKM/Galon\tCombustible costo x km\t\tGastos varios x km\tPrecio x galon\t\tCosto Total\t\tKM\n");
 
+    for (float consumo = consumo_max; consumo >= consumo_min; consumo -= precision)
+        {
+        km_por_galon = litros_por_galon * 100 / consumo;
+        consumo_promedio = (consumo_max + consumo_min) / 2;
+        costo_total = precio_combustible * km_recorridos / km_por_galon + gastos_totales;
+
+        printf("%.1f\t\t\t%s\t\t%.2f\t\t\t%.2f\t\t\t\t%.2f\t\t\t%.2f\t\t%.2f\t\t\t%.2f\t\t\n",
+            consumo_promedio, cliente, km_por_galon, precio_combustible * km_recorridos / km_por_galon,
+            gastos_varios_km, precio_combustible, costo_total, km_recorridos);
+    }
+
+    fclose(archivo);
+    printf("\n\nLos resultados se han guardado en el archivo resultados.txt.\n");
     printf("\n-----GRACIAS POR CONFIAR EN NOSOTROS, VUELVA PRONTO-----\n\n");
 
-
- return 0;
+    return 0;
 }
